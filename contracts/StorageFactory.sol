5598ec9ebcc0e28c24e2024e09e477b95148d860
khaddyBola <khadijahmusa232@gmail.com>
2024-01-19 10:21:14 +0100

    all commit

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,14 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "/SimpleStorage.sol";
+import "./SimpleStorage.sol";
 
 
 contract StorageFactory {
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public simpleStorageArray;
     
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+       SimpleStorage simpleStorage = new SimpleStorage();
+       simpleStorageArray.push(simpleStorage);
     }
+    
 }
 
