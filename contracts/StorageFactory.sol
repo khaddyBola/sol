85979d4816077179bf28055150b63b84f8a01e14
khaddyBola <khadijahmusa232@gmail.com>
2024-01-19 12:08:30 +0100

    storage commit

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -11,6 +11,17 @@ contract StorageFactory {
        SimpleStorage simpleStorage = new SimpleStorage();
        simpleStorageArray.push(simpleStorage);
     }
-    
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // Address
+        // ABI - Application Binary Interface
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
+    }
 }
 
