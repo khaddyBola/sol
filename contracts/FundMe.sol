4bbaffccff61cd6ab0cd11fd3b18340fe0861558
khaddyBola <khadijahmusa232@gmail.com>
2024-01-19 14:31:30 +0100

    extra commit

---
+++ contracts/FundMe.sol
@@ -0,0 +1,16 @@
+// Get funds from users
+// Withdraw funds
+// Set a minimum funding value in USD
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract FundMe {
+    function fund() public {
+        // want to be able to set a minimum fund amount in USD
+        // 1. How do we send ETH to this correct?
+    }
+
+    // function withdraw(){}
+}
+
