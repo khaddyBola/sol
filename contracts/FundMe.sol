5d42f3bf4164c456d47ba86723564edf2010f946
khaddyBola <khadijahmusa232@gmail.com>
2024-01-20 11:11:24 +0100

    extra commit

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -6,9 +6,12 @@
 pragma solidity ^0.8.8;
 
 contract FundMe {
-    function fund() public {
+    uint256 public minimumUsd = 50;
+
+    function fund() public payable {
         // want to be able to set a minimum fund amount in USD
         // 1. How do we send ETH to this correct?
+        require(msg.value >= minimumUsd, "Didn't send enough!"); //1e18 == 1 * 10 ** 18
     }
 
     // function withdraw(){}
