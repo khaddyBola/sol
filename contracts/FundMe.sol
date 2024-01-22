f2f7dc9bf255b4fc6e46de017ff0273f1aa6e6d2
khaddyBola <khadijahmusa232@gmail.com>
2024-01-22 15:41:17 +0100

    initial commit

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,36 +5,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./Price"
 
 contract FundMe {
-    uint256 public minimumUsd = 5;
+
+    uint256 public minimumUsd = 50 * 1e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
 
     function fund() public payable {
+       
         // want to be able to set a minimum fund amount in USD
         // 1. How do we send ETH to this correct?
-        require(msg.value >= minimumUsd, "Didn't send enough!"); //1e18 == 1 * 10 ** 18 == 1000000000000
-        //18 decimals 
-    }
-
-        function getPrice() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price,,,) = priceFeed.latestRoundData(); 
-        // ETH IN TERMS OF USD
-        // 3000.0000000
-
-        return uint256(price * 1e10); // 1**10 == 10000000000
-    }
-
-    function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd; 
+        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough!"); //1e18 == 1 * 10 ** 18 == 1000000000000
+        funders.push(msg.sender);
+        addressToAmountFunded(msg.sender) = msg.value;
     }
 
     // function withdraw(){}
