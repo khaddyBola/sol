ba7823f6b35662862dc7607c5fd2c3653f939022
khaddyBola <khadijahmusa232@gmail.com>
2024-01-23 11:56:25 +0100

    changes

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,9 +5,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "./Price";
+import "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
 
     uint256 public minimumUsd = 50 * 1e18;
 
@@ -15,10 +16,7 @@ contract FundMe {
     mapping(address => uint256) public addressToAmountFunded;
 
     function fund() public payable {
-       
-        // want to be able to set a minimum fund amount in USD
-        // 1. How do we send ETH to this correct?
-        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough!"); //1e18 == 1 * 10 ** 18 == 1000000000000
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!"); //1e18 == 1 * 10 ** 18 == 1000000000000
         funders.push(msg.sender);
         addressToAmountFunded(msg.sender) = msg.value;
     }
