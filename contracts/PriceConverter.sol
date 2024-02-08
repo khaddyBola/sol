df2e68758463a7074bff4ed591ff36050cdb46ae
khaddyBola <khadijahmusa232@gmail.com>
2024-02-08 12:27:53 +0100

    converter

--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -20,7 +20,7 @@ library PriceConverter {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         return priceFeed.version();
     }
-
+     
     function getConversionRate(uint256 ethAmount) internal view returns (uint256) {
         uint256 ethPrice = getPrice();
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
